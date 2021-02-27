ce4cf04e8678d4302b50c651a44c241bfda80fdf
AshwinAzhagiri <ashwin.azhagiri@hcl.com>
2021-02-27 14:10:50 -0500

    Credit Check Project Created
    Spring Initializr - Spring Web dependencies, Spring Boot 2.4.3, Java 8

---
+++ CreditCheck/src/test/java/com/hcl/CreditCheck/CreditCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.CreditCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
